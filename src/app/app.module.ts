9ddf032162d571dc0ba18466d8f900585d78b08e
Ricardo-Sema <ricardo.oliveira@sema.ce.gov.br>
2023-02-22 22:48:26 -0300

    commit inicial

---
+++ src/app/app.module.ts
@@ -0,0 +1,40 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http'
+import { FormsModule } from '@angular/forms'
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+import { TemplateModule } from './template/template.module';
+import { HomeComponent } from './home/home.component'
+import { LoginComponent } from './login/login.component';
+import { LayoutComponent } from './layout/layout.component'
+import { AuthService } from './auth.service';
+import { TokenInterceptor } from './token.interceptor';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    LayoutComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    AppRoutingModule,
+    TemplateModule
+  ],
+  providers: [
+    AuthService,
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: TokenInterceptor,
+      multi: true
+    }
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
